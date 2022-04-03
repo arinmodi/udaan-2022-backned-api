a58085974dab8e15e8fae11a550cb035df78a992
Arin Modi <arinmodi2306@gmail.com>
2022-04-03 14:27:37 +0530

    chnaged the get api

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,6 @@ const getVotes = require('./getVotes');
 const schema = require('./validation');
 
 router.patch('/vote', validate(schema) ,vote);
-router.get('/vote', getVotes);
+router.get('/fetch/udaan_22/votes', getVotes);
 
 module.exports = router;
